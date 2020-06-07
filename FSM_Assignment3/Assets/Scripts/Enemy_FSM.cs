dff97971d413163f64c2f95a04c4745413400844
MWaqas1512 <64679036+MWaqas1512@users.noreply.github.com>
2020-06-07 17:36:38 +0500

    Assignment Submission on sue date
    This is the assignment 3. I have done the code which i can.

---
+++ FSM_Assignment3/Assets/Scripts/Enemy_FSM.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+using UnityEngine.UI;
+public class Enemy_FSM : MonoBehaviour
+{
+    public enum ENEMY_STATE { PATROL, CHASE, ATTACK };
+    [SerializeField]
+    private ENEMY_STATE currentState;
+
+    public ENEMY_STATE CurrentState
+    {
+        get
+        {
+            return currentState;
+
+        }
+        set
+        {
+
+            currentState = value;
+            StopAllCoroutines();
+            switch (currentState)
+            {
+                case ENEMY_STATE.PATROL:
+                    StartCoroutine(EnemyPatrol());
+                    break;
+                case ENEMY_STATE.CHASE:
+                    StartCoroutine(ChasePlayer());
+                    break;
+            }
+
+        }
+    }
+
+    private CheckMyVision checkMyVision;
+    private NavMeshAgent agent = null;
+    public Transform playerTransform = null;
+    private Transform patrolDestination = null;
+    private Health playerHealth = null;
+    public float maxDamage = 10f;
+    private void Awake()
+    {
+        checkMyVision = GetComponent<CheckMyVision>();
+        agent = GetComponent<NavMeshAgent>();
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        playerTransform = playerHealth.GetComponent<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] destinations = GameObject.FindGameObjectsWithTag("Dest");
+        patrolDestination = destinations[Random.Range(0, destinations.Length)].GetComponent<Transform>();
+        CurrentState = ENEMY_STATE.PATROL;
+    }
+
+    public IEnumerator EnemyPatrol()
+    {
+        while (currentState == ENEMY_STATE.PATROL)
+        {
+            checkMyVision.sensitity = CheckMyVision.enmSensitivity.HIGH;
+            agent.isStopped = false;
+            agent.SetDestination(patrolDestination.position);
+            while (agent.pathPending)
+            {
+                yield return null;
+            }
+
+            if (checkMyVision.targetInSight)
+            {
+                agent.isStopped = true;
+                CurrentState = ENEMY_STATE.CHASE;
+                yield break;
+            }
+            yield return null;
+        }
+
+    }
+    public IEnumerator ChasePlayer()
+    {
+        while (currentState == ENEMY_STATE.CHASE)
+        {
+            checkMyVision.sensitity = CheckMyVision.enmSensitivity.LOW;
+            agent.isStopped = false;
+            agent.SetDestination(checkMyVision.lastknownSighting);
+            while (agent.pathPending)
+            {
+                yield return null;
+            }
+           
+                playerHealth.HealthPoints -= maxDamage * Time.deltaTime;
+                yield return null;
+        }
+        yield break;
+    }
+}
---
+++ FSM_Assignment3/Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public float HealthPoints
+    {
+        get
+        {
+            return healthPoints;
+        }
+        set
+        {
+            healthPoints = value;
+            
+            // What if we reach 0?
+            if(healthPoints <= 0)
+            {
+                life--;
+                if (life >= 1)
+                {
+                    healthPoints = 100f;
+                }
+                else
+                {
+                    Destroy(gameObject);   
+                }
+            }
+        }
+    }
+
+    [SerializeField]
+    private float healthPoints = 100f;
+    [SerializeField]
+    private float life = 3.0f;
+}
